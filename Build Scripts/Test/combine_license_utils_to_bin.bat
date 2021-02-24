bdd3829acc07c08c6a834a4f97ef34beb7cb2c05
risatech\tomp <tomp@risa.com>
2021-02-24 09:33:11 -0800

    initial pass for license manager MSM

---
+++ Build Scripts/Test/combine_license_utils_to_bin.bat
@@ -0,0 +1,5 @@
+cd "..\..\Source Files\Common Files\License\Utilities"
+2> nul mkdir bin
+xcopy "Change License Type\*.*" "bin\" /Y
+xcopy "File Uploader\*.*" "bin\" /Y
+xcopy "Licensing Dashboard\*.*" "bin\" /Y
