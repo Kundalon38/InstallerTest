036a2c385546004e90d878b9050109a3d59f52f2
Kundalon38 <alyosha1955@yahoo.com>
2021-03-05 12:08:53 -0800

    Finished Service Start / Stop CAs

--- Projects/Common/Custom Actions/RISA_CustomActionsLib/RISA_CustomActionsLib/CustomAction.cs
+++ Projects/Common/Custom Actions/RISA_CustomActionsLib/RISA_CustomActionsLib/CustomAction.cs
@@ -1,6 +1,9 @@
 using Microsoft.Deployment.WindowsInstaller;
 using System;
 using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Threading;
 
 namespace RISA_CustomActionsLib
 {
@@ -10,6 +13,8 @@ namespace RISA_CustomActionsLib
     //
     public class CustomActions
     {
+        #region DetectRoaming
+
         [CustomAction]
         public static ActionResult DetectRoaming(Session session)
         {
@@ -41,5 +46,85 @@ namespace RISA_CustomActionsLib
 
             return ActionResult.Success;
         }
+
+        #endregion
+
+        #region Stop / Start RISA32 Service
+
+        //
+        // The Services applet says the Name + Description are "RISA32 License Manager Service";
+        //      the service name, used below, is RISA32
+        //
+        // Code was cribbed from LicensingDashboard, where it's implemented using async/await,
+        //      however that version and this one simply:
+        //          issue stop/start service,
+        //          then: sleep / poll <-repeat until service is in desired state.
+        //      Re-implement here using old school Thread.Sleep to avoid client interfacing problems with async/await
+        //
+        private const string _risaServiceName = "RISA32";
+        private const int _sleep_millisecs = 500;
+        private const int _num_SleepsPerSecond = 2;
+        private const int _maxWaitForService_Seconds = 60;
+
+        [CustomAction]
+        public static  ActionResult ServiceControlStopRISA32(Session session)
+        {
+            var services = ServiceController.GetServices();
+
+            var risaSvc = services.FirstOrDefault(x => x.ServiceName == _risaServiceName);
+
+            if (risaSvc == null) return ActionResult.Success;                                   // service isn't installed
+            if (risaSvc.Status != ServiceControllerStatus.Running) return ActionResult.Success; // service isn't running
+            //
+            // stop service
+            //
+            try
+            {
+                risaSvc.Stop();             // asynchronous, call returns before service actually stops
+
+                for (var nWaits = 0; nWaits < _maxWaitForService_Seconds * _num_SleepsPerSecond; nWaits++)
+                {
+                    Thread.Sleep(_sleep_millisecs);
+                    if (risaSvc.Status == ServiceControllerStatus.Stopped) return ActionResult.Success;
+                }
+            }
+            catch
+            {
+            }
+            return ActionResult.Failure;
+        }
+
+        [CustomAction]
+        public static ActionResult ServiceControlStartRISA32(Session session)
+        {
+            // always return Success, and is fire n forget - don't wait for service to fully start
+            //
+            var services = ServiceController.GetServices();
+
+            var risaSvc = services.FirstOrDefault(x => x.ServiceName == _risaServiceName);
+
+            if (risaSvc == null) return ActionResult.Success;                                   // service isn't installed
+            if (risaSvc.Status == ServiceControllerStatus.Running) return ActionResult.Success; // service is already running
+            //
+            // start service
+            //
+            try
+            {
+                risaSvc.Start();             // asynchronous, call returns before service actually starts
+
+            //    for (var nWaits = 0; nWaits < _maxWaitForService_Seconds * _num_SleepsPerSecond; nWaits++)
+            //    {
+            //        Thread.Sleep(_sleep_millisecs);
+            //        if (risaSvc.Status == ServiceControllerStatus.Running) return ActionResult.Success;
+            //    }
+            }
+            catch
+            {
+            }
+            return ActionResult.Success;
+        }
+
+        #endregion
+
     }
 }
