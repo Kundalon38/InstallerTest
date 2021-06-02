d777335b80399babdb7868db0339e49858b0dea6
Kundalon38 <alyosha1955@yahoo.com>
2021-06-02 13:14:04 -0700

    WIP / Restore Point

--- Projects/Common/Custom Actions/RISA_CustomActionsLib/RISA_CustomActionsLib.Test/SiValidate_Tests.cs
+++ Projects/Common/Custom Actions/RISA_CustomActionsLib/RISA_CustomActionsLib.Test/SiValidate_Tests.cs
@@ -4,11 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RISA_CustomActionsLib.Models.Linked;
 
 namespace RISA_CustomActionsLib.Test
 {
     [TestClass]
     public class SiValidate_Tests : TestBase
     {
+        [TestMethod, TestCategory("SiValidate")]
+        public void CmdLine_Loud_OK()
+        {
+            var cmdLine = $@"install_3d_1900.exe";
+            var btd = new BootstrapperTestData()
+            {
+                CmdLine = cmdLine
+            };
+            var siRes = CustomActions.SilentValidate(btd);
+
+            expecting(bd.IsSilent);
+            bd.ParseCmdLine();
+            expecting(bd.CmdLineProperties.Count == 1);
+            expecting(bd.CmdLineProperties[0].PropName == myProp);
+            expecting(bd.CmdLineProperties[0].PropValue == myPropValue);
+            Assert.IsTrue(true);
+        }
+
+
     }
 }
---
+++ Projects/Common/Custom Actions/RISA_CustomActionsLib/RISA_CustomActionsLib/Models/Linked/SilentResult.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Deployment.WindowsInstaller;
+
+namespace RISA_CustomActionsLib.Models.Linked
+{
+    // used by testing to look at everything
+    // is similar to a Tuple<>, which isn't available in .NET 4.5
+    //
+    public class SilentResult
+    {
+        public ActionResult Result { get; }
+        public BootstrapperData BooData { get; }
+
+        private SilentResult(ActionResult ar, BootstrapperData booData)
+        {
+            Result = ar;
+            BooData = booData;
+        }
+
+        public static SilentResult OK(BootstrapperData booData)
+        {
+            return new SilentResult(ActionResult.Success, booData);
+        }
+
+        public static SilentResult Fail (BootstrapperData booData)
+        {
+            return new SilentResult(ActionResult.Failure, booData);
+        }
+    }
+}
