a7287beea694be4e701cca3900f50d24143aee01
risatech\tomp <tomp@risa.com>
2021-06-07 15:27:20 -0700

    created command line build scripts - these will rebuild dependent merge modules

---
+++ Projects/RISA-3D_dotNET/Standalone/build.bat
@@ -0,0 +1,16 @@
+@call "C:\Advanced Installer\Projects\RISA License Manager\build.bat"
+@call "C:\Advanced Installer\Projects\Common\Merge Modules\RISA Core Files\build.bat"
+@if "%~1" == "" goto default
+set builds=%1
+goto st
+:default
+set builds="Standalone"
+:st
+@pushd "c:\Program Files (x86)\Caphyon\Advanced Installer 18.3\bin\x86"
+@set proj="C:\Advanced Installer\Projects\RISA-3D_dotNET\Standalone\RISA-3D_dotNET.aip"
+advancedinstaller /edit %proj% /SetProperty ProductVersion="19.0.2.63472"
+advancedinstaller /edit %proj% /SetProperty RISA_PRODUCT_VERSION2="19.0"
+advancedinstaller /edit %proj% /SetProperty RISA_PRODUCT_VERSION34="2.63472"
+advancedinstaller /edit %proj% /SetProperty RISA_PRODUCT_TITLE2_INSTYPE="RISA-3D 19.0"
+advancedInstaller /rebuild %proj% -buildslist %builds%
+@popd
---
+++ Projects/RISAFoundation/Standalone/build.bat
@@ -0,0 +1,16 @@
+@call "C:\Advanced Installer\Projects\RISA License Manager\build.bat"
+@call "C:\Advanced Installer\Projects\Common\Merge Modules\RISA Core Files\build.bat"
+@if "%~1" == "" goto default
+set builds=%1
+goto st
+:default
+set builds="Standalone"
+:st
+@pushd "c:\Program Files (x86)\Caphyon\Advanced Installer 18.3\bin\x86"
+@set proj="C:\Advanced Installer\Projects\RISAFoundation\Standalone\RISAFoundation.aip"
+advancedinstaller /edit %proj% /SetProperty ProductVersion="13.0.0.1"
+advancedinstaller /edit %proj% /SetProperty RISA_PRODUCT_VERSION2="13.0"
+advancedinstaller /edit %proj% /SetProperty RISA_PRODUCT_VERSION34="0.1"
+advancedinstaller /edit %proj% /SetProperty RISA_PRODUCT_TITLE2_INSTYPE="RISA Foundation 13.0"
+advancedInstaller /rebuild %proj% -buildslist %builds%
+@popd
