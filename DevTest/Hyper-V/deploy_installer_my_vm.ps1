a1f0601011068d6194229e712f67160d6ecad2af
risatech\tomp <tomp@risa.com>
2021-06-10 10:11:30 -0700

    fixed deployment script to choose most recent file

--- DevTest/Hyper-V/deploy_installer_my_vm.ps1
+++ DevTest/Hyper-V/deploy_installer_my_vm.ps1
@@ -9,23 +9,23 @@ Switch ($num)
 {
     "3D" {
     	$SrcFolder = "C:\Advanced Installer\Projects\RISA-3D_dotNET\Standalone\Setup Files"
-      	foreach ($File in (Get-ChildItem $SrcFolder -recurse | ? Mode -ne 'd-----')){
-        	Copy-VMFile -VM (Get-VM $VMName) -SourcePath $file.fullname -DestinationPath "$DestFolder$file" -FileSource Host -CreateFullPath -Force}
+	$File = Get-ChildItem -Path $SrcFolder -Name *.exe | Sort-Object LastAccessTime -Descending | Select-Object -First 1
+       	Copy-VMFile -VM (Get-VM $VMName) -SourcePath $SrcFolder"\"$File -DestinationPath "$DestFolder$file" -FileSource Host -CreateFullPath -Force
     }
     "FD" {
     	$SrcFolder = "C:\Advanced Installer\Projects\RISAFoundation\Standalone\Setup Files"
-      	foreach ($File in (Get-ChildItem $SrcFolder -recurse | ? Mode -ne 'd-----')){
-        	Copy-VMFile -VM (Get-VM $VMName) -SourcePath $file.fullname -DestinationPath "$DestFolder$file" -FileSource Host -CreateFullPath -Force}
+	$File = Get-ChildItem -Path $SrcFolder -Name *.exe | Sort-Object LastAccessTime -Descending | Select-Object -First 1
+       	Copy-VMFile -VM (Get-VM $VMName) -SourcePath $SrcFolder"\"$File -DestinationPath "$DestFolder$file" -FileSource Host -CreateFullPath -Force
     }
     "FL" {
     	$SrcFolder = "C:\Advanced Installer\Projects\RISAFloor\Standalone\Setup Files"
-      	foreach ($File in (Get-ChildItem $SrcFolder -recurse | ? Mode -ne 'd-----')){
-        	Copy-VMFile -VM (Get-VM $VMName) -SourcePath $file.fullname -DestinationPath "$DestFolder$file" -FileSource Host -CreateFullPath -Force}
+	$File = Get-ChildItem -Path $SrcFolder -Name *.exe | Sort-Object LastAccessTime -Descending | Select-Object -First 1
+       	Copy-VMFile -VM (Get-VM $VMName) -SourcePath $SrcFolder"\"$File -DestinationPath "$DestFolder$file" -FileSource Host -CreateFullPath -Force
     }
     "CN" {
     	$SrcFolder = "C:\Advanced Installer\Projects\RISAConnection\Standalone\Setup Files"
-      	foreach ($File in (Get-ChildItem $SrcFolder -recurse | ? Mode -ne 'd-----')){
-        	Copy-VMFile -VM (Get-VM $VMName) -SourcePath $file.fullname -DestinationPath "$DestFolder$file" -FileSource Host -CreateFullPath -Force}
+	$File = Get-ChildItem -Path $SrcFolder -Name *.exe | Sort-Object LastAccessTime -Descending | Select-Object -First 1
+       	Copy-VMFile -VM (Get-VM $VMName) -SourcePath $SrcFolder"\"$File -DestinationPath "$DestFolder$file" -FileSource Host -CreateFullPath -Force
     }
     default { "Unknown product" }
 }
