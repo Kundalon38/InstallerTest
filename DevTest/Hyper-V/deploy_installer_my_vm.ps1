f0073151b710e5fbcad9fe5cd044abe685184baf
risatech\tomp <tomp@risa.com>
2021-06-18 12:24:01 -0700

    fixed powershell create date issue

--- DevTest/Hyper-V/deploy_installer_my_vm.ps1
+++ DevTest/Hyper-V/deploy_installer_my_vm.ps1
@@ -9,31 +9,31 @@ Switch ($num)
 {
     "3D" {
     	$SrcFolder = "C:\Advanced Installer\Projects\RISA-3D_dotNET\Standalone\Setup Files"
-	$File = Get-ChildItem -Path $SrcFolder -Name *.exe | Sort-Object LastWriteTime -Descending | Select-Object -First 1
+	$File = Get-ChildItem -Path $SrcFolder -Filter "*.exe" | Sort-Object LastWriteTime -Descending | Select-Object -First 1
 	Write-Output "Copying $File to virtual machine $VMName..."
        	Copy-VMFile -VM (Get-VM $VMName) -SourcePath $SrcFolder"\"$File -DestinationPath "$DestFolder$file" -FileSource Host -CreateFullPath -Force
     }
     "FD" {
     	$SrcFolder = "C:\Advanced Installer\Projects\RISAFoundation\Standalone\Setup Files"
-	$File = Get-ChildItem -Path $SrcFolder -Name *.exe | Sort-Object LastWriteTime -Descending | Select-Object -First 1
+	$File = Get-ChildItem -Path $SrcFolder -Filter "*.exe" | Sort-Object LastWriteTime -Descending | Select-Object -First 1
 	Write-Output "Copying $File to virtual machine $VMName..."
        	Copy-VMFile -VM (Get-VM $VMName) -SourcePath $SrcFolder"\"$File -DestinationPath "$DestFolder$file" -FileSource Host -CreateFullPath -Force
     }
     "FL" {
     	$SrcFolder = "C:\Advanced Installer\Projects\RISAFloor\Standalone\SetupFiles"
-	$File = Get-ChildItem -Path $SrcFolder -Name *.exe | Sort-Object LastWriteTime -Descending | Select-Object -First 1
+	$File = Get-ChildItem -Path $SrcFolder -Filter "*.exe" | Sort-Object LastWriteTime -Descending | Select-Object -First 1
 	Write-Output "Copying $File to virtual machine $VMName..."
        	Copy-VMFile -VM (Get-VM $VMName) -SourcePath $SrcFolder"\"$File -DestinationPath "$DestFolder$file" -FileSource Host -CreateFullPath -Force
     }
     "CN" {
     	$SrcFolder = "C:\Advanced Installer\Projects\RISAConnection\Standalone\Setup Files"
-	$File = Get-ChildItem -Path $SrcFolder -Name *.exe | Sort-Object LastWriteTime -Descending | Select-Object -First 1
+	$File = Get-ChildItem -Path $SrcFolder -Filter "*.exe" | Sort-Object LastWriteTime -Descending | Select-Object -First 1
 	Write-Output "Copying $File to virtual machine $VMName..."
        	Copy-VMFile -VM (Get-VM $VMName) -SourcePath $SrcFolder"\"$File -DestinationPath "$DestFolder$file" -FileSource Host -CreateFullPath -Force
     }
     "LM" {
     	$SrcFolder = "C:\Advanced Installer\Projects\RISA License Manager\RISA License Manager Dev-SetupFiles"
-	$File = Get-ChildItem -Path $SrcFolder -Name *.exe | Sort-Object LastWriteTime -Descending | Select-Object -First 1
+	$File = Get-ChildItem -Path $SrcFolder -Filter "*.exe" | Sort-Object LastWriteTime -Descending | Select-Object -First 1
 	Write-Output "Copying $File to virtual machine $VMName..."
        	Copy-VMFile -VM (Get-VM $VMName) -SourcePath $SrcFolder"\"$File -DestinationPath "$DestFolder$file" -FileSource Host -CreateFullPath -Force
     }
